ee459788e9ceeaa6c6a341576d43ceedaa6ff91d
Tina Tran <tinatran@Tinas-iMac.hsd1.il.comcast.net>
2022-11-30 14:17:10 -0600

    refractor list of attendees

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,34 +1,18 @@
 import React from 'react';
 import Nav from './Nav';
+import AttendeesList from './AttendeesList';
 
 function App(props) {
   if (props.attendees === undefined){
     return null;
   }
   return(
-    <React.Fragment>
-    <Nav />
-    <div className="container">
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Conference</th>
-          </tr>
-        </thead>
-        <tbody>
-          {props.attendees?.map(attendee => {
-            return (
-            <tr key={attendee.href}>
-              <td>{ attendee.name }</td>
-              <td>{ attendee.conference }</td>
-            </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
-    </React.Fragment>
+    <>
+      <Nav />
+      <div className="container">
+        <AttendeesList attendees={props.attendees} />
+      </div>
+    </>
   );
 }
 
