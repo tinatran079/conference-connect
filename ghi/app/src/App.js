daf89f6d59d9aff96e9f3992e0a449bc196adf20
Tina Tran <tinatran@Tinas-iMac.hsd1.il.comcast.net>
2022-12-01 18:47:15 -0600

    added main page

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -5,20 +5,23 @@ import LocationForm from './LocationForm';
 import ConferenceForm from './ConferenceForm';
 import AttendConferenceForm from './AttendConferenceForm';
 import PresentationForm from './PresentationForm';
+import MainPage from './MainPage';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 
 function App(props) {
+  if (props.attendees === undefined) {
+    return null;
+  }
   return (
     <BrowserRouter>
       <Nav />
       <Routes>
+      <Route index element={<MainPage />} />
         <Route path="locations">
-          {/* <Route path="" element={<LocationList />} /> */}
           <Route path="new" element={<LocationForm />} />
         </Route>
         <Route path="conferences">
-          {/* <Route path="" element={<ConferenceList />} /> */}
           <Route path="new" element={<ConferenceForm />} />
         </Route>
         <Route path="attendees">
